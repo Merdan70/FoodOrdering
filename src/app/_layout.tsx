71bd1d020f7fed2be2c755771d8b2a7dc762f91b
merdan.momin@gmail.com <merdan@Merdans-iMac.home>
2024-09-12 15:53:34 +0200

    Update and delete a Product

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,7 +6,8 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/src/components/useColorScheme';
+import { useColorScheme } from '@components/useColorScheme';
+import CartProvider from '@/providers/CartProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -50,10 +51,13 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <CartProvider >
+        <Stack>
+          <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+          <Stack.Screen name="(user)" options={{ headerShown: false }} />
+          <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
+        </Stack>
+      </CartProvider>
     </ThemeProvider>
   );
 }
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,53 @@
+import Colors from 'src/constants/Colors';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { Product } from '../types';
+import { Link, useSegments } from 'expo-router';
+import ProductDetailsScreen from '@/app/(user)/menu/[id]';
+
+export const defaultPizzaImage= 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+type ProductListItemProps ={
+    product: Product;
+}
+
+const ProductListItem = ({product}: ProductListItemProps) => {
+  const segments = useSegments();
+
+  return (
+    <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
+        <Pressable  style={styles.container}>
+        <Image source={{uri:product.image || defaultPizzaImage}} 
+        style={styles.image}
+        resizeMode='contain'
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}> €{product.price}</Text>
+    
+        </Pressable>
+    </Link>
+  )
+}
+
+
+export default  ProductListItem;
+const styles = StyleSheet.create({
+  container: {
+   backgroundColor:'white',
+   padding: 10,
+   borderRadius: 20,
+   flex: 1,
+   maxWidth: '50%',
+  },
+  title: {
+    fontSize:20, 
+    fontWeight:'600',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    aspectRatio:1,
+  },
+});
