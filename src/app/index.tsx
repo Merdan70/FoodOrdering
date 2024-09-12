c958a4ea9949c1e888eb71383ff19102e3b77758
merdan.momin@gmail.com <merdan@Merdans-iMac.home>
2024-09-12 17:27:55 +0200

    Assignment Auto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,12 +6,11 @@ import { Link } from 'expo-router';
 const index = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
-        <Button text="User" />
-      </Link>
-      <Link href={'/(admin)'} asChild>
-        <Button text="Admin" />
-      </Link>
+      
+    <Link href={'/sign-in'} asChild>
+      <Button text="Sign in" />
+    </Link>
+      
     </View>
   );
 };
